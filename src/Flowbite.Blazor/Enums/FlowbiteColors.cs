c1493f88afa4db9cd5e4b82a70cde57ef529d2ad
Denny09310 <k.denny2000@gmail.com>
2025-01-10 12:08:57 +0100

    feat: added utilities, added icons, added js initializers

---
+++ src/Flowbite.Blazor/Enums/FlowbiteColors.cs
@@ -0,0 +1,13 @@
+﻿namespace Flowbite.Blazor.Enums;
+
+public enum FlowbiteColors
+{
+    Gray,
+    Red,
+    Yellow,
+    Green,
+    Blue,
+    Indigo,
+    Purple,
+    Pink,
+}
--- src/Flowbite.Blazor/wwwroot/Flowbite.Blazor.lib.module.js
+++ src/Flowbite.Blazor/wwwroot/Flowbite.Blazor.lib.module.js
@@ -1,7 +1,45 @@
 ﻿(() => {
-    const script = document.createElement('script');
-    script.src = '_content/Flowbite.Blazor/lib/flowbite/dist/flowbite.min.js';
-    script.type = 'text/javascript';
-    script.async = true;
-    document.body.appendChild(script);
-})()
+    const FLOWBITE_SCRIPT_SRC = '_content/Flowbite.Blazor/lib/flowbite/dist/flowbite.min.js';
+
+    // Load the Flowbite script
+    const loadFlowbiteScript = () => {
+        const script = document.createElement('script');
+        script.src = FLOWBITE_SCRIPT_SRC;
+        script.async = true;
+        script.onload = () => {
+            if (typeof initFlowbite === 'function') {
+                initFlowbite();
+            }
+        };
+        document.body.appendChild(script);
+    };
+
+    // Debounce utility function
+    const debounce = (func, delay) => {
+        let timeoutId;
+        return (...args) => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => func(...args), delay);
+        };
+    };
+
+    // Initialize Flowbite with debouncing
+    const initializeFlowbite = debounce(() => {
+        if (typeof initFlowbite === 'function') {
+            initFlowbite();
+        }
+    }, 300);
+
+    // Observe DOM changes to reinitialize Flowbite
+    const observeDOMChanges = () => {
+        const observer = new MutationObserver(initializeFlowbite);
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    };
+
+    // Execute script loading and observer setup
+    loadFlowbiteScript();
+    observeDOMChanges();
+})();
