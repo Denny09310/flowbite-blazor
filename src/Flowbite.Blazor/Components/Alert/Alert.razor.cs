dcd9f058c82af40c78086c560acbc5a36bad01c1
Denny09310 <k.denny2000@gmail.com>
2025-01-10 14:30:58 +0100

    chore: changed id creation for alert

--- src/Flowbite.Blazor/Components/Alert/Alert.razor.cs
+++ src/Flowbite.Blazor/Components/Alert/Alert.razor.cs
@@ -14,7 +14,7 @@ namespace Flowbite.Blazor.Components;
 /// </remarks>
 public partial class Alert : FlowbiteComponentBase
 {
-    public Alert() => Id ??= Identifier.NewId();
+    public Alert() => Id ??= $"alert-{Identifier.NewId()}";
 
     private string DataDismissTarget => $"#{Id}";
 
@@ -45,7 +45,7 @@ public partial class Alert : FlowbiteComponentBase
     /// Gets or sets the color of the alert.
     /// </summary>
     [Parameter]
-    public FlowbiteColors Color { get; set; }
+    public Colors Color { get; set; }
 
     /// <summary>
     /// Gets or sets a value indicating whether the alert is dismissable.
@@ -97,40 +97,40 @@ public partial class Alert : FlowbiteComponentBase
 
     private string? SelectBorderVariant() => Color switch
     {
-        FlowbiteColors.Gray => "border-gray-500",
-        FlowbiteColors.Red => "border-red-500",
-        FlowbiteColors.Yellow => "border-yellow-500",
-        FlowbiteColors.Green => "border-green-500",
-        FlowbiteColors.Blue => "border-blue-500",
-        FlowbiteColors.Indigo => "border-indigo-500",
-        FlowbiteColors.Purple => "border-purple-500",
-        FlowbiteColors.Pink => "border-pink-500",
+        Colors.Gray => "border-gray-500",
+        Colors.Red => "border-red-500",
+        Colors.Yellow => "border-yellow-500",
+        Colors.Green => "border-green-500",
+        Colors.Blue => "border-blue-500",
+        Colors.Indigo => "border-indigo-500",
+        Colors.Purple => "border-purple-500",
+        Colors.Pink => "border-pink-500",
         _ => throw new NotImplementedException(),
     };
 
     private string? SelecteCloseVariant() => Color switch
     {
-        FlowbiteColors.Gray => "text-gray-700 bg-gray-100 dark:bg-gray-200 dark:text-gray-800",
-        FlowbiteColors.Red => "text-red-700 bg-red-100 dark:bg-red-200 dark:text-red-800",
-        FlowbiteColors.Yellow => "text-yellow-700 bg-yellow-100 dark:bg-yellow-200 dark:text-yellow-800",
-        FlowbiteColors.Green => "text-green-700 bg-green-100 dark:bg-green-200 dark:text-green-800",
-        FlowbiteColors.Blue => "text-blue-700 bg-blue-100 dark:bg-blue-200 dark:text-blue-800",
-        FlowbiteColors.Indigo => "text-indigo-700 bg-indigo-100 dark:bg-indigo-200 dark:text-indigo-800",
-        FlowbiteColors.Purple => "text-purple-700 bg-purple-100 dark:bg-purple-200 dark:text-purple-800",
-        FlowbiteColors.Pink => "text-pink-700 bg-pink-100 dark:bg-pink-200 dark:text-pink-800",
+        Colors.Gray => "text-gray-700 bg-gray-100 dark:bg-gray-200 dark:text-gray-800",
+        Colors.Red => "text-red-700 bg-red-100 dark:bg-red-200 dark:text-red-800",
+        Colors.Yellow => "text-yellow-700 bg-yellow-100 dark:bg-yellow-200 dark:text-yellow-800",
+        Colors.Green => "text-green-700 bg-green-100 dark:bg-green-200 dark:text-green-800",
+        Colors.Blue => "text-blue-700 bg-blue-100 dark:bg-blue-200 dark:text-blue-800",
+        Colors.Indigo => "text-indigo-700 bg-indigo-100 dark:bg-indigo-200 dark:text-indigo-800",
+        Colors.Purple => "text-purple-700 bg-purple-100 dark:bg-purple-200 dark:text-purple-800",
+        Colors.Pink => "text-pink-700 bg-pink-100 dark:bg-pink-200 dark:text-pink-800",
         _ => throw new NotImplementedException(),
     };
 
     private string? SelectVariant() => Color switch
     {
-        FlowbiteColors.Gray => "bg-gray-100 text-gray-500 focus:ring-gray-400 hover:bg-gray-200 dark:bg-gray-200 dark:text-gray-600 dark:hover:bg-gray-300",
-        FlowbiteColors.Red => "bg-red-100 text-red-500 focus:ring-red-400 hover:bg-red-200 dark:bg-red-200 dark:text-red-600 dark:hover:bg-red-300",
-        FlowbiteColors.Yellow => "bg-yellow-100 text-yellow-500 focus:ring-yellow-400 hover:bg-yellow-200 dark:bg-yellow-200 dark:text-yellow-600 dark:hover:bg-yellow-300",
-        FlowbiteColors.Green => "bg-green-100 text-green-500 focus:ring-green-400 hover:bg-green-200 dark:bg-green-200 dark:text-green-600 dark:hover:bg-green-300",
-        FlowbiteColors.Blue => "bg-blue-100 text-blue-500 focus:ring-blue-400 hover:bg-blue-200 dark:bg-blue-200 dark:text-blue-600 dark:hover:bg-blue-300",
-        FlowbiteColors.Indigo => "bg-indigo-100 text-indigo-500 focus:ring-indigo-400 hover:bg-indigo-200 dark:bg-indigo-200 dark:text-indigo-600 dark:hover:bg-indigo-300",
-        FlowbiteColors.Purple => "bg-purple-100 text-purple-500 focus:ring-purple-400 hover:bg-purple-200 dark:bg-purple-200 dark:text-purple-600 dark:hover:bg-purple-300",
-        FlowbiteColors.Pink => "bg-pink-100 text-pink-500 focus:ring-pink-400 hover:bg-pink-200 dark:bg-pink-200 dark:text-pink-600 dark:hover:bg-pink-300",
+        Colors.Gray => "bg-gray-100 text-gray-500 focus:ring-gray-400 hover:bg-gray-200 dark:bg-gray-200 dark:text-gray-600 dark:hover:bg-gray-300",
+        Colors.Red => "bg-red-100 text-red-500 focus:ring-red-400 hover:bg-red-200 dark:bg-red-200 dark:text-red-600 dark:hover:bg-red-300",
+        Colors.Yellow => "bg-yellow-100 text-yellow-500 focus:ring-yellow-400 hover:bg-yellow-200 dark:bg-yellow-200 dark:text-yellow-600 dark:hover:bg-yellow-300",
+        Colors.Green => "bg-green-100 text-green-500 focus:ring-green-400 hover:bg-green-200 dark:bg-green-200 dark:text-green-600 dark:hover:bg-green-300",
+        Colors.Blue => "bg-blue-100 text-blue-500 focus:ring-blue-400 hover:bg-blue-200 dark:bg-blue-200 dark:text-blue-600 dark:hover:bg-blue-300",
+        Colors.Indigo => "bg-indigo-100 text-indigo-500 focus:ring-indigo-400 hover:bg-indigo-200 dark:bg-indigo-200 dark:text-indigo-600 dark:hover:bg-indigo-300",
+        Colors.Purple => "bg-purple-100 text-purple-500 focus:ring-purple-400 hover:bg-purple-200 dark:bg-purple-200 dark:text-purple-600 dark:hover:bg-purple-300",
+        Colors.Pink => "bg-pink-100 text-pink-500 focus:ring-pink-400 hover:bg-pink-200 dark:bg-pink-200 dark:text-pink-600 dark:hover:bg-pink-300",
         _ => throw new NotImplementedException()
     };
 
