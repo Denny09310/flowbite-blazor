693febb1074dfe76dd0f6fdb7c993f186962f9e6
Denny09310 <k.denny2000@gmail.com>
2025-01-13 08:57:34 +0100

    chore: moved IdentifierContext inside Identifier file

--- src/Flowbite.Blazor/Utilities/Identifier.cs
+++ src/Flowbite.Blazor/Utilities/Identifier.cs
@@ -6,12 +6,6 @@ public static class Identifier
     private static readonly Random _rnd = new();
 
     /// <summary>
-    /// Returns a new <see cref="IdentifierContext"/> where ID are sequential: "f0000", "f0001", "f0002", ...
-    /// </summary>
-    /// <returns></returns>
-    public static IdentifierContext SequentialContext() => new((n) => $"f{n:0000}");
-
-    /// <summary>
     /// Returns a new small Id.
     /// HTML id must start with a letter.
     /// Example: f127d9edf14385adb
@@ -41,6 +35,12 @@ public static class Identifier
         return IdentifierContext.Current.GenerateId();
     }
 
+    /// <summary>
+    /// Returns a new <see cref="IdentifierContext"/> where ID are sequential: "f0000", "f0001", "f0002", ...
+    /// </summary>
+    /// <returns></returns>
+    public static IdentifierContext SequentialContext() => new((n) => $"f{n:0000}");
+
     /* There are several solutions to generate a small identifier.
        Here you will find examples and time estimates.
        ----------------------------------------------------------------------
@@ -48,7 +48,7 @@ public static class Identifier
        BenchmarkDotNet=v0.13.1, OS=Windows 11
        Intel Core i7-1065G7 CPU 1.30GHz, 1 CPU, 8 logical and 4 physical cores
        DefaultJob : .NET 6.0.3 (6.0.322.12309), X64 RyuJIT
-    
+
        | Method           | Example                              |  Mean     |
        |------------------|--------------------------------------|-----------|
        | From Rnd Long:     10aad4a0                                51.14 ns |
@@ -57,9 +57,65 @@ public static class Identifier
        | From Ticks:        8da188a929219fab                       174.41 ns |
        | From Guid Base64:  aRk3NU0b0ka04Oqc                       206.67 ns |
        | From Guid:         25ee3842-0c33-4a63-9b83-a7d1c0266809   224.59 ns |
-       | From Guid Shorted: 6ee0e232196b4a9                        237.51 ns | 
-     
+       | From Guid Shorted: 6ee0e232196b4a9                        237.51 ns |
+
        PS: Ticks does not always generate unique values in WASM apps.
 
      */
+}
+
+// Inspiration: https://dvoituron.com/2020/01/22/UnitTest-DateTime/
+
+public sealed class IdentifierContext : IDisposable
+{
+    private static readonly ThreadLocal<Stack<IdentifierContext>> _threadScopeStack = new(() => new Stack<IdentifierContext>());
+
+    public IdentifierContext(Func<uint, string> newId)
+    {
+        _threadScopeStack.Value?.Push(this);
+        NewId = newId;
+        CurrentIndex = 0;
+    }
+
+    public static IdentifierContext? Current
+    {
+        get
+        {
+            if (_threadScopeStack.Value == null || _threadScopeStack.Value.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return _threadScopeStack.Value?.Peek();
+            }
+        }
+    }
+
+    private uint CurrentIndex { get; set; }
+
+    private Func<uint, string> NewId { get; init; }
+
+    public void Dispose()
+    {
+        _ = _threadScopeStack.Value?.TryPop(out _);
+    }
+
+    /// <summary>
+    /// Returns the next number between 0 and 99999999.
+    /// </summary>
+    /// <returns></returns>
+    internal string GenerateId()
+    {
+        var id = NewId.Invoke(CurrentIndex);
+
+        CurrentIndex++;
+
+        if (CurrentIndex >= 99999999)
+        {
+            CurrentIndex = 0;
+        }
+
+        return id;
+    }
 }
