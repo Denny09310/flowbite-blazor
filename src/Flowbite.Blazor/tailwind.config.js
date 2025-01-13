1e0d4b8763aaa39c1fc962be6353ebc87baaa860
Denny09310 <k.denny2000@gmail.com>
2025-01-13 15:08:20 +0100

    feat: removed dark mode 'class'

--- src/Flowbite.Blazor/tailwind.config.js
+++ src/Flowbite.Blazor/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
     content: [
         './{Components,Icons}/**/*.{razor,cs}',
         "./node_modules/flowbite/**/*.js"
